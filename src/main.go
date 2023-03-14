5a8d676be589e42b2851a4649bb5c8af99f5b59f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-03-14 16:23:03 -0700

    healthz checks and array

--- src/main.go
+++ src/main.go
@@ -1,30 +1,78 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/jackc/pgx/v5"
 )
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the Assessor API! Made by Kyler Chin https://github.com/kylerchin")
+// Parcel represents a row in the parcels table
+type Parcel struct {
+	Address string  `json:"address"`
+	Lat     float64 `json:"lat"`
+	Long    float64 `json:"long"`
 }
+
+// GetParcelCoords retrieves the lat and long for a given address from the database
+func GetParcelCoords(w http.ResponseWriter, r *http.Request) {
+	address := r.URL.Query().Get("address")
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("ASSESSOR_DB_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "SELECT saddr as address, center_lat as lat, center_lon as long FROM parcels WHERE saddr = $1", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var parcels []Parcel
+	for rows.Next() {
+		var parcel Parcel
+		if err := rows.Scan(&parcel.Address, &parcel.Lat, &parcel.Long); err != nil {
+			log.Fatal(err)
+		}
+		parcels = append(parcels, parcel)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(parcels)
+}
+
+
+// Healthz returns a 200 OK status code to indicate that the service is healthy and ready to serve requests
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	conn, err := pgx.Connect(context.Background(),  os.Getenv("ASSESSOR_DB_URL"))
+	if err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+	defer conn.Close(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if err := conn.Ping(ctx); err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 
 func main() {
-// Connect  to database
-// urlExample := "postgres://username:password@localhost:5432/database_name"
-conn, err := pgx.Connect(context.Background(), os.Getenv("ASSESSOR_DB_URL"))
-if err != nil {
-	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-	os.Exit(1)
-}
-defer conn.Close(context.Background())
-
-
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", homeLink)
-
-    log.Fatal(http.ListenAndServe(":8080", router))
+	http.HandleFunc("/getcoords", GetParcelCoords)
+	http.HandleFunc("/healthz", Healthz)
+	fmt.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
