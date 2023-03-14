2afa77bf7b19b92009f7de5a8ae970cee630533c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-03-14 15:55:50 -0700

    add go

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/mux"
+	"github.com/jackc/pgx/v5"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +13,18 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+// Connect  to database
+// urlExample := "postgres://username:password@localhost:5432/database_name"
+conn, err := pgx.Connect(context.Background(), os.Getenv("ASSESSOR_DB_URL"))
+if err != nil {
+	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+	os.Exit(1)
+}
+defer conn.Close(context.Background())
+
+
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", homeLink)
+
     log.Fatal(http.ListenAndServe(":8080", router))
 }
